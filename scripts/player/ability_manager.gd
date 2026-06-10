19a9cf21f894da7af14e1478dafd434ab9be35c1
Bau <bautistadelprato16@gmail.com>
2026-06-10 17:00:29 -0300

    attack animation + colliders

--- scripts/player/ability_manager.gd
+++ scripts/player/ability_manager.gd
@@ -1,8 +1,23 @@
 extends Node2D
 #CLASE MANEJADORA DE HABILIDADES BÁSICAS (ataque, bloqueo y dash)
+@onready var Attackbox=$"../Attackbox"
+@onready var animation=$"../AnimatedSprite2D"
 
 func attack():
-	print("Attack")
+
+	var player = get_parent()
+
+	player.is_attacking = true
+
+	animation.play("lyra_basic_attack")
+
+	Attackbox.monitoring = true
+
+	await animation.animation_finished
+
+	Attackbox.monitoring = false
+
+	player.is_attacking = false
 
 func dash():
 	print("Dash")
--- scripts/player/character_body_2d.gd
+++ scripts/player/character_body_2d.gd
@@ -1,38 +1,43 @@
 extends CharacterBody2D
 
-@onready var animation=$AnimatedSprite2D
-@onready var lyra_health=$HealthManager #import para salud
-@onready var lyra_ability=$AbilityManager #import para habilidades
+@onready var animation = $AnimatedSprite2D
+@onready var lyra_health = $HealthManager
+@onready var lyra_ability = $AbilityManager
+
+var is_attacking = false
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta):
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 
+	if Input.is_action_just_pressed("attack") and !is_attacking:
+		$AbilityManager.attack()
+
 	if direction > 0:
 		animation.flip_h = false
 	elif direction < 0:
 		animation.flip_h = true
-	
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	update_animation(direction)
+	if !is_attacking:
+		update_animation(direction)
 
 	move_and_slide()
 
 func update_animation(direction):
-	
-	
-	
+
 	if not is_on_floor():
-		animation.play("lyra_idle") #CAMBIAR POR ANIMACION DE SALTO
+		animation.play("lyra_idle") # cambiar por salto después
 	elif direction != 0:
 		animation.play("lyra_walk")
 	else:
