a9fae40629f62ae1947666b512e1b70f7e5ad86c
morawess <morawesselsandia@gmail.com>
2026-06-17 15:05:11 -0300

    Revert "mini juego"
    This reverts commit 2a382683b8579917992472c959d1984ae0d779c3.

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -7,8 +7,8 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta):
-	# Replace with function body.
+func _process(delta: float) -> void:
+	pass
 
 
 func _on_empezar_pressed() -> void:
@@ -19,13 +19,5 @@ func _on_opciones_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/OptionsScreen.tscn") # Replace with function body.
 
 
-func _on_salir_pressed():
-	# Primero verificamos que el nodo esté conectado al árbol del juego
-	if get_tree() != null:
-		get_tree().quit()
-	else:
-		print("Error: El nodo no está en el SceneTree, pero el juego se cerrará igual.")
-
-
-func _on_minijuego_pressed() -> void:
-	get_tree().change_scene_to_file("res://miniJuego/scenes/Game.tscn")
+func _on_salir_pressed() -> void:
+	get_tree().quit() # Replace with function body.
