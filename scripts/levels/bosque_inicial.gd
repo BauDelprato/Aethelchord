b2bb8cb93b2ebb7f66c38286d3acf147a6041c6f
morawess <morawesselsandia@gmail.com>
2026-06-21 23:58:37 -0300

    dialogue en el bosque

---
+++ scripts/levels/bosque_inicial.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var intro_dialogue: DialogueResource 
+const GloboArriba = preload("res://resources/dialogues/balloon_top.tscn")
+
+func _ready():
+	pass # etc
+
+func _on_trigger_intro_body_entered(body):
+	if body.name == "Lyra":
+		var globo = GloboArriba.instantiate()
+		add_child(globo)
+		globo.start(intro_dialogue, "start")
+		$triggerIntro.queue_free()
