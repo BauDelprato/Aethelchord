d84b552b8c4fefa0a7310c2f423fc335a41a6eeb
morawess <morawesselsandia@gmail.com>
2026-06-21 22:39:33 -0300

    fusion con version agus

---
+++ scenes/levels/bosque_inicial.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	
+#	$TileMapLayer/Lyra/Camera2DCamera2D.limit_left = 0
+#	$TileMapLayer/Lyra/Camera2DCamera2D.limit_right = 5750
