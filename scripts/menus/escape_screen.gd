df86684189f4b3e9c460d725a69f6a5bbf4d780e
Bau <bautistadelprato16@gmail.com>
2026-06-27 14:14:36 -0300

    fix pausa

--- scripts/menus/escape_screen.gd
+++ scripts/menus/escape_screen.gd
@@ -4,6 +4,7 @@ var paused := false
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS #nodo está funcionando todo el tiempo
+	
 	hide()
 
 func _input(event):
@@ -27,11 +28,9 @@ func _on_continue_pressed():
 	resume()
 
 func _on_options_pressed():
-	get_tree().paused = false
-	paused = false
+	resume()
 	get_tree().change_scene_to_file("res://scenes/menus/options.tscn")
 
 func _on_exit_pressed():
-	get_tree().paused = false
-	paused = false
+	resume()
 	get_tree().change_scene_to_file("res://scenes/menus/titleScreen.tscn")
